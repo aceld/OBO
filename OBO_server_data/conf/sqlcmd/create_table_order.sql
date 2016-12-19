3b791ad7665fe8a02735fe03478d180adc8b5818
aceld <danbing_at@163.com>
2016-12-19 10:11:34 +0800

    添加创建订单表接口

--- OBO_server_data/conf/sqlcmd/create_table_order.sql
+++ OBO_server_data/conf/sqlcmd/create_table_order.sql
@@ -2,11 +2,11 @@ create table OBO_TABLE_ORDER
 (
     orderid                 VARCHAR(64) not null primary key, 
     passenger_username      VARCHAR(32), 
-    driver_usrename         VARCHAR(32), 
+    driver_username         VARCHAR(32), 
 
     create_order_time       timestamp not NULL DEFAULT CURRENT_TIMESTAMP, 
-    start_order_time        timestamp not NULL, 
-    end_time                timestamp not NULL, 
+    start_order_time        timestamp, 
+    end_time                timestamp, 
 
     src_address             VARCHAR(256), 
     dst_address             VARCHAR(256), 
